733a47991a2e5297d8ddcb91e337d75e9a61203b
alex <alex.zh255@gmail.com>
2025-02-15 22:30:15 +0200

    fixed heroku config

--- config.php
+++ config.php
@@ -11,17 +11,10 @@ $host = 'm60mxazb4g6sb4nn.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
 $username = 'te8lov4gn7unchmo';
 $password = 'cikvmz4nutkopf8c';
 $dbname = 'gf5hvtcaepa1vs0q';
-
-// $conn = new mysqli($host, $username, $password, $dbname, 3306);
-
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-// echo "Connected successfully";
+$port = 3306;
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [$port = 3306]);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [$port]);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
